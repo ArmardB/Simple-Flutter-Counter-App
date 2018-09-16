9a60e95941ed0f54397f8fee79eec7550b3d6149
Armard Bellamy <armardbellamy@gmail.com>
2018-09-16 11:39:31 -0400

    Add state and app bar

--- lib/ui/fun_money.dart
+++ lib/ui/fun_money.dart
@@ -3,16 +3,23 @@ import 'package:flutter/material.dart';
 class FunMoney extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
+    return new FunMoneyState();
   }
 
+
+  
+}
+
+class FunMoneyState extends State<FunMoney> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Fun Money'),
+        backgroundColor: Colors.lightGreen,
       ),
     );
   }
-  
+
 }
+
