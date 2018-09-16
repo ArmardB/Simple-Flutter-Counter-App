d3246fdfe86b0ddf78f14c60930495c582a90b74
Armard Bellamy <armardbellamy@gmail.com>
2018-09-16 12:34:51 -0400

    Added button to reset counter and ternary color settings

--- lib/ui/fun_money.dart
+++ lib/ui/fun_money.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class FunMoney extends StatefulWidget {
   @override
@@ -9,6 +10,19 @@ class FunMoney extends StatefulWidget {
 }
 
 class FunMoneyState extends State<FunMoney> {
+
+  int _moneyCounter = 0;
+
+  void _stackThatMoney() {
+    setState(() {
+      _moneyCounter = _moneyCounter + 100;
+    });
+  }
+
+  void _clearCounter() {
+    _moneyCounter = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -23,8 +37,29 @@ class FunMoneyState extends State<FunMoney> {
                 child: new Text("Get Rich!",
                   style: new TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 29.9)),
             ),
-
-
+            new Expanded(
+                child: new Center(
+                  child: new Text('\$$_moneyCounter',
+                  style: new TextStyle(
+                    color: _moneyCounter < 5000 ? Colors.red.shade200 : Colors.green ,
+                      fontSize: 46.9,
+                      fontWeight: FontWeight.w800
+                  )),
+                )),
+            new Center(
+              child: new FlatButton(
+                  color: Colors.lightGreen,
+                  textColor: Colors.white70,
+                  onPressed: () => _stackThatMoney(),
+                  child: new Text("Ball Out",
+                    style: new TextStyle(fontSize: 19.9),)),
+            ),
+             new Center(
+              child: new FlatButton(
+                  color: Colors.red,
+                  textColor: Colors.white70,
+                  onPressed: _clearCounter, child: new Text('Clear', style: new TextStyle(fontSize: 19.9),)),
+            )
           ],
         ),
       ),
