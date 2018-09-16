27fb0fcad79ef676e3f425757cf48bfd837066f7
Armard Bellamy <armardbellamy@gmail.com>
2018-09-16 11:32:46 -0400

    Add boilerplate code

---
+++ lib/ui/fun_money.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FunMoney extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Fun Money'),
+      ),
+    );
+  }
+  
+}
